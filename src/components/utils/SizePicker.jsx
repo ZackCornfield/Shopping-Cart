9c11c74c365887f9f3e119c3ce0817b75f33cb2b
ZackCornfield <139100711+ZackCornfield@users.noreply.github.com>
2025-01-15 14:03:15 +0800

    .

--- src/components/utils/SizePicker.jsx
+++ src/components/utils/SizePicker.jsx
@@ -2,8 +2,7 @@ import React, { useState } from 'react'
 
 const SizePicker = () => {
     const [active, setActive] = useState(9);
-    const sizes = [5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12, 12.5, 13, 13.5, 14, 14.5];
-
+    const sizes = [5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12];
     return (
         <div className='size-picker'>
             <h3 className='title'>choose size</h3>
